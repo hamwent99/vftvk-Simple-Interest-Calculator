6a8ffadf310ba9c1095b3042521211912ff1c1c8
Hamwent Rawat <hamwent.rawat@borngroup.com>
2022-10-21 15:04:05 +0530

    changes ongoing

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+
+var principal = document.getElementById("principal").value;    
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+
+// update interst rate over range slider
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+// compute your simple interst
 function compute()
 {
     p = document.getElementById("principal").value;
     
-}
-        
+}      
