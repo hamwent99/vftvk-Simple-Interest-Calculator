eeacf7a3ad51531e8ccec340c664ff4c66ce07b0
Hamwent Rawat <hamwent.rawat@borngroup.com>
2022-11-01 18:01:04 +0530

    calc code testing

--- script.js
+++ script.js
@@ -6,6 +6,19 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
+
+// years option
+function selectNumOfYears() {
+  // var x = document.getElementById("years");
+  // var i = x.selectedIndex;
+  // document.getElementById("demo").innerHTML = x.options[i].text;
+    var x = document.getElementById("years").selectedIndex;
+  var y = document.getElementById("years").options;
+  // alert("Index: " + y[x].index + " is " + y[x].text);
+    document.getElementById("checkyear").value = y[x].text;
+  alert(document.getElementById("checkyear").value);
+
+}
 //compute your simple interst
 function calculate(){
     var principal = 0;
@@ -18,7 +31,7 @@ function calculate(){
     var rate = document.getElementById("rate").value;
     document.getElementById("int_rate").innerHTML=rate;
 
-    var years = document.getElementById("years").value;
+    var years = document.getElementById("checkyear").value;
     var year = new Date().getFullYear()+parseInt(years);    
     document.getElementById("for_years").innerHTML=year;
 
