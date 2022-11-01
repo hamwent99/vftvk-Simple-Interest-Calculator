fc6e63006858561aeb96fa2231b4499704b657d4
Hamwent Rawat <hamwent.rawat@borngroup.com>
2022-11-01 15:57:45 +0530

    calc code testing

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 
-var principal = document.getElementById("principal").value;    
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
 
 // update interst rate over range slider
 function updateRate() 
@@ -11,9 +6,33 @@ function updateRate()
     var rateval = document.getElementById("rate").value;
     document.getElementById("rate_val").innerText=rateval;
 }
-// compute your simple interst
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}      
+//compute your simple interst
+function calculate(){
+    var principal = 0;
+    var years = 0;
+    var rate = 0;
+    var SI= 0;    
+    var principal = document.getElementById("principal").value;    
+    document.getElementById("p_amt").innerHTML=principal;
+
+    var rate = document.getElementById("rate").value;
+    document.getElementById("int_rate").innerHTML=rate;
+
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear()+parseInt(years);    
+    document.getElementById("for_years").innerHTML=year;
+
+    var SI = principal * years * rate /100;
+
+    SI = (principal*years*rate)/100;
+    document.getElementById("res").innerHTML=SI;
+
+    var element = document.getElementById('to-show');
+    if (element.classList.contains('hide') && principal != null && years != null && SI != null && SI != null) {
+        element.classList.remove("hide");
+        element.classList.add("show");
+    } else {
+        element.classList.remove("show");
+        element.classList.add("hide");
+    }
+}
